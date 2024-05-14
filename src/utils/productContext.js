f04f1dbb0797c9ce32a48e68f821a4c0afb3dace
dhanjyoti <dhanjyotir20@gmail.com>
2024-05-14 20:52:02 +0530

    WIP

--- src/utils/productContext.js
+++ src/utils/productContext.js
@@ -1,9 +1,38 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import reducer from "./productReducer"
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts: []
+}
+
 const AppProvider = ({children}) => {
-    return <AppContext.Provider value={{myName:"dhan"}}>{children}</AppContext.Provider>
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const getProducts = async (url)=>{
+        dispatch({type: "SET_LOADING"});
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            dispatch({type: "SET_API_DATA", payload: products});
+        } catch (error) {
+            dispatch({type:"API_ERROR"});
+        }
+    }
+
+    useEffect(()=>{
+        getProducts(API)
+    },[])
+    return (
+        <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    )
 };
 
 const useProductContext = ()=>{
